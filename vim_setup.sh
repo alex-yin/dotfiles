253ef99564f2bf2083290d0d658a1d141ea0309c
Alex Yin <zixuan.yin@mail.mcgill.ca>
2018-05-01 12:08:58 -0400

    mapo

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 cd ~
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-
 cp ~/myvimrc/.vimrc  ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

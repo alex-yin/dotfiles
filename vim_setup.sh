a41ad3d9f2ee56dcb0041a60dde0d7fc1e7246f3
Alex Yin <zixuan.yin@mail.mcgill.ca>
2018-05-14 10:15:10 -0400

    bangbang

--- vim_setup.sh
+++ vim_setup.sh
@@ -6,7 +6,7 @@ cp .vimrc  ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+git clone --recurse-submodules -j8  https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py
 cd ~

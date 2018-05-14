3b6b612213e6302606ffc74058c1aa79d563b1d3
Alex Yin <zixuan.yin@mail.mcgill.ca>
2018-05-14 11:16:44 -0400

    pumba

--- vim_setup.sh
+++ vim_setup.sh
@@ -8,6 +8,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 git clone --recurse-submodules -j8  https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe
-./install.py
+./install.py --clang-completer
 cd ~
 
